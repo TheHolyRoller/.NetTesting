20d0417ad0984725a9123d21917ed64efdaea26e
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-01-29 11:29:05 +0100

    Codewars setup

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Add in the imports here 
+using System;
+using System.Text.RegularExpressions; 
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Text;
+
+
+
+Console.WriteLine("Hello, World!");
