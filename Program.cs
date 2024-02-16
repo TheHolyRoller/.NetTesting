511bd1a200daa1eedabd9eb2349f9c83a7b297bf
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-02-16 20:13:11 +0100

    Letter Testing

--- Program.cs
+++ Program.cs
@@ -7,86 +7,49 @@ using System.Text;
 
 /*
 
-Okay so it seems as though the task 
-
-is pretty simple I just need to return true 
-
-if the animal is bringing a disk that has a name with it's 
-starting and ending letters the same as the starting and ending letters of 
-their name 
-
-So basically I'm given the name of an animal in a string and 
-I'm given the name of a dish in a string and I need to determine if 
-
-the starting and ending letters match the starting and ending letters 
-
-of the dish. 
-
-Okay so this should be quite simple. 
-
-I'll turn both the animal name and the dish name into character Arrays 
-
-Then I'll take the first and last character from the Animal Name and the 
-first and last character from the dish name. 
-
-I'll return true if they match up and false if they don't. 
-
+    
+    if(Char.isLetter(a) && Char.isLetter(b) && Char.isUpper(a) && Char.isUpper(b)){
 
 */
 
+char a = 'a';
+char b = 'B';
 
-// Create the input animal name here 
-string beast = "great blue heron";
+bool isLetterA = Char.IsLetter(a);
+bool isLetterB = Char.IsLetter(b);
 
-string dish = "garlic naan";
-
-
-// Turn the names to char Arrays here 
-char[] beastArray = beast.ToCharArray();
-
-char[] dishArray = dish.ToCharArray();
+bool isUpperA = Char.IsUpper(a);
+bool isUpperB = Char.IsUpper(b); 
 
 
-// Capture the first element of beastArray here 
-char beastFirst = beastArray[0];
-
-char beastLast = beastArray[beastArray.Length - 1];
+if(!isLetterA || !isLetterB){
 
 
-char dishFirst = dishArray[0];
-char dishLast = dishArray[dishArray.Length - 1]; 
-
-
-
-if(beastFirst == dishFirst && beastLast == dishLast){
-
-
-    Console.WriteLine("this is the first char of Beast name");
-    Console.WriteLine(beastFirst);
-
-    Console.WriteLine("this is the first char of dish");
-    Console.WriteLine(dishFirst);
-
-
-    Console.WriteLine("this is the last char of beast");
-    Console.WriteLine(beastLast);
-    Console.WriteLine("this is the last char of dish");
-    Console.WriteLine(dishLast); 
+    Console.WriteLine("one of the characters is not a letter"); 
     
-    Console.WriteLine("they match up!"); 
-        
-        
+
+}
+
+else if(isLetterA && isLetterB && isUpperA && isUpperB){
+
+
+    Console.WriteLine("They are both letters and uppercase"); 
+
+}
+
+if(isLetterA && isLetterB && !isUpperA && !isUpperB){
 
 
 }
 
-else{
 
 
-    Console.WriteLine("they don't match"); 
-    
+if(isLetterA && isLetterB && !isUpperA || !isUpperB){
+
+    Console.WriteLine("Both characters are letters but not uppercase"); 
 
 }
+
 
 
 
